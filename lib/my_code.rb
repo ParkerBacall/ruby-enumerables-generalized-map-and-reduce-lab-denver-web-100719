b88244367708cdb331d440b6a70668ae99b01b8e
Parker Bacall <pbacall2@gmail.com>
2019-10-02 01:18:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,8 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    yield(source_array[i]
+    yield(source_array[i])
     i += 1
   end
-  return new)
+  return new
 end
