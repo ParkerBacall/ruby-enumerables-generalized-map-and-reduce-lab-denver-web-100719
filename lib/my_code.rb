bd33dcf13bf19ca140a5019fa41d78f89f3835cc
Parker Bacall <pbacall2@gmail.com>
2019-10-02 01:26:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map(source_array)
   end
   new
 end
+
+def reduce(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield (source_array[i]))
+    i += 1
+  end
+  new
+end
