98b3656bd203e27e76721a8483b9b77d91f341d9
Parker Bacall <pbacall2@gmail.com>
2019-10-02 01:13:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,12 @@
 # Your Code Here
 def map(source_array)
-  yield (new = []
+  new = []
   i = 0
   while i < source_array.length do
-    #new.push( source_array[i] * -1 ) # <== Unique work
+    yeild
     i += 1
   end
-  return new)
+  return new
 end
 
 map(source_array) |innards|
