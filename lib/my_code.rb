3b5cfef6a196c356b1d99420a4311e3dd368b489
Parker Bacall <pbacall2@gmail.com>
2019-10-02 01:34:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,7 +18,7 @@ def reduce(source_array, sv=nil)
     i = 1
   end
   i = 0
-  while i < source_array.length do
+  while i < source_array.length 
     sum = yield(sum, source_array[i])
     i += 1
   end
